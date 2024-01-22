2945ec5e2d6d057a66cbdbfc6bfb743019a9b0a2
Memorysaver <mfa1484@gmail.com>
2024-01-23 01:28:23 +0800

    imp game cards

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -3,9 +3,10 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
 // https://api.rawg.io/docs/#operation/games_list
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 
 interface FetchGameResponse {
