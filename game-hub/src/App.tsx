02a46fff2d6f053c6152ef0cf6dd6429547c2b18
Memorysaver <mfa1484@gmail.com>
2024-01-22 19:27:50 +0800

    build responsive layout

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,6 +1,27 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    // Chakra breakpoint https://chakra-ui.com/docs/styled-system/responsive-styles
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, //larger than 1024x
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
